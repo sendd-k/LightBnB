175ab31706f2db4cf059e46e3b4cf850bd7282e6
Kevin Holmes <kevinholmes921@gmail.com>
2022-06-02 19:07:51 +0000

    refactoring addUSER

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -85,12 +85,32 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+
+const addUser = (user) => {
+
+  pool
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3) RETURNING *;` ,
+      [user.name, user.email, user.password]) 
+      
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      //console.log(err.message);
+      return null;
+    });
+};
+
 exports.addUser = addUser;
 
 /// Reservations
